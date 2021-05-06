7274a4c15b421f7dd1f7a44365f02c5d8245031e
Sanal Somasundaran <sanal.somasundaran@flex.com>
2021-05-06 18:37:13 +0530

    push notification update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { NewsletterService } from './services/newsletter.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cowin-app';
+
+  readonly VAPID_PUBLIC_KEY = "BKIRcLVVlfUgcaYUPM3xq-BSOrsQmiBxY0IObQ5yb9LcgtvTZNxK-7xVuQ5IrN222DSlrhnMqlNw2Qs7r7-N7Cc";
+
+  center = {
+    "centers": [
+      {
+        "center_id": 574616, "name": "Jejuri RH Local 18 To 44 Only", "address": "Rural Hospital Jejuri", "state_name": "Maharashtra", "district_name": "Pune", "block_name": "Purandar", "pincode": 412303, "lat": 18, "long": 74, "from": "10:00:00", "to": "14:00:00", "fee_type": "Free",
+        "sessions": [
+          {
+            "session_id": "2d0be6c3-5891-4feb-8cda-95abd2d81ebb", "date": "06-05-2021", "available_capacity": 0, "min_age_limit": 18, "vaccine": "COVISHIELD", "slots":
+              ["10:00AM-11:00AM", "11:00AM-12:00PM", "12:00PM-01:00PM", "01:00PM-02:00PM"]
+          }]
+      }]
+  }
+
+  constructor(private swPush: SwPush,
+    private newsletterService: NewsletterService) {
+
+  }
+
+  subscribeToNotifications() {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+    .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+    .catch(err => console.error("Could not subscribe to notifications", err));
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NewsletterService } from './services/newsletter.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true, //environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [
+    NewsletterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
